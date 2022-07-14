1f23789a7cd95b2d886dcb87b5e1bc7451083738
飘辰 <piaochen.xing@trustbe.cn>
2022-07-14 09:59:02 +0800

    add crypto test

---
+++ hello_crypto/src/main.rs
@@ -0,0 +1,25 @@
+use blake2::{Blake2b512, Blake2s256, Digest};
+use hex_literal::hex;
+
+use std::time::{Duration, Instant};
+use std::thread::sleep;
+
+
+fn test_blake2() {
+    println!("start");
+    let now = Instant::now();
+    sleep(Duration::new(2, 0));
+    for n in 0..1000000 {
+        let mut hasher = Blake2s256::new();
+        hasher.update(b"hello world");
+        let res = hasher.finalize();
+    }
+    println!("{}", now.elapsed().as_millis());
+  
+    
+}
+
+
+fn main() {
+  test_blake2();
+}
